17ec3ddd05cf0025fb02986212c2b43c3b682390
edgar615 <edgar615@gmail.com>
2014-07-03 21:55:18 +0800

    将IE8的脚步移动到ie8.js中

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -1,39 +1,4 @@
 'use strict';
-if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-        return this.replace(/^\s+|\s+$/g, '');
-    };
-}
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function (searchElement, fromIndex) {
-        if ( this === undefined || this === null ) {
-            throw new TypeError( '"this" is null or not defined' );
-        }
-
-        var length = this.length >>> 0; // Hack to convert object.length to a UInt32
-
-        fromIndex = +fromIndex || 0;
-
-        if (Math.abs(fromIndex) === Infinity) {
-            fromIndex = 0;
-        }
-
-        if (fromIndex < 0) {
-            fromIndex += length;
-            if (fromIndex < 0) {
-                fromIndex = 0;
-            }
-        }
-
-        for (;fromIndex < length; fromIndex++) {
-            if (this[fromIndex] === searchElement) {
-                return fromIndex;
-            }
-        }
-
-        return -1;
-    };
-}
 
 angular
     .module('rootApp', ["ngRoute", "app.directives", "app.filters", "routeResolverServices", "app.services", "pascalprecht.translate" ])
