073da69bd1597be1be0975dbc8610d84828a53f6
edgar615 <edgar615@gmail.com>
2014-07-25 16:42:37 +0800

    增加activiti

--- src/main/java/com/edgar/core/workflow/WorkflowProcessor.java
+++ src/main/java/com/edgar/core/workflow/WorkflowProcessor.java
@@ -1,13 +1,31 @@
 package com.edgar.core.workflow;
 
+import java.util.Iterator;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class WorkflowProcessor {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(WorkflowProcessor.class);
+	
 	private List<Activity> activities;
 
 	public void doWorkflow() throws Exception {
+		LOGGER.info("activities: {}", activities);
 		ProcessorContext ctx = new ProcessorContext();
+		
+		List<Activity> actlist = getActivities();
+		if (actlist != null) {
+			Iterator<Activity> i = actlist.iterator();
+			while (i.hasNext()) {
+				Activity a = (Activity) i.next();
+//				ctx = a.execute(ctx);
+				a.getHandler();
+			}
+		}
+		
 		doWorkflow(ctx);
 	}
 
