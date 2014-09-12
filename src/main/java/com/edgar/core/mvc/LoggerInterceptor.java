beb188d112134bd1b6b46d2894dbdd23db30506c
edgar615 <edgar615@gmail.com>
2014-09-12 10:44:30 +0800

    修改日志

--- src/main/java/com/edgar/core/mvc/LoggerInterceptor.java
+++ src/main/java/com/edgar/core/mvc/LoggerInterceptor.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +17,32 @@ import java.util.Map;
  * Created by Administrator on 14-9-12.
  */
 public class LoggerInterceptor extends HandlerInterceptorAdapter {
+
+    private long beginTime;
+    private long handerTime;
+    private long wallClockTime;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (handler instanceof HandlerMethod) {
+            beginTime = System.currentTimeMillis();
+        }
+
+        return super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            handerTime = System.currentTimeMillis() - beginTime;
+        }
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            wallClockTime = System.currentTimeMillis() - beginTime;
             HandlerMethod method = (HandlerMethod) handler;
             Logger logger = LoggerFactory.getLogger(method.getBeanType());
             AuthHelper authHelper = method.getMethodAnnotation(AuthHelper.class);
@@ -34,15 +58,14 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
                 }
             }
             if (authHelper != null) {
-                logger.info("{},url:{}, httpMethod:{},httpParam:{},ip:{}", authHelper.value(), request.getRequestURL(), request.getMethod(), map,
-                        getIpAddr(request));
+                logger.info("{},url:{}, httpMethod:{},httpParam:{},ip:{}, wallClockTime: {}, handerTime: {}", authHelper.value(), request.getRequestURL(), request.getMethod(), map,
+                        getIpAddr(request), wallClockTime, handerTime);
             } else {
-                logger.info("url:{}, httpMethod:{},httpParam:{},ip:{}", request.getRequestURL(), request.getMethod(), map,
-                        getIpAddr(request));
+                logger.info("url:{}, httpMethod:{},httpParam:{},ip:{}, wallClockTime: {}, handerTime: {}", request.getRequestURL(), request.getMethod(), map,
+                        getIpAddr(request), wallClockTime, handerTime);
             }
         }
-
-        return super.preHandle(request, response, handler);
+        super.afterCompletion(request, response, handler, ex);
     }
 
     private String getIpAddr(HttpServletRequest request) {
